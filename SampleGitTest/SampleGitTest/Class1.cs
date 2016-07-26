f77d82db5253154cf81be4c327a8e7467034679e
abhigokhale <abhigokhale@gmail.com>
2016-07-26 17:54:17 +0530

    Added initial application codebase

---
+++ SampleGitTest/SampleGitTest/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleGitTest
+{
+    public class Class1
+    {
+        //Sample app to test Git features
+    }
+}
